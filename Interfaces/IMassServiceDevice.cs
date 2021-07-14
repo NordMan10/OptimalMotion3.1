10286a0c57060c398be3d739e523818cbd6f86b3
Yury Ten <yury.ten66@yandex.ru>
2021-07-14 15:50:16 +0300

    Base structure is complete

---
+++ Interfaces/IMassServiceDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimalMotion3._1.Interfaces
+{
+    public interface IMassServiceDevice
+    {
+
+    }
+}
